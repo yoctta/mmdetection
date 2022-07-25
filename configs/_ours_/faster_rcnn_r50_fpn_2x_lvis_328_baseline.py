5157fe9b87e2920c7a7ad3cecefa999fe5952412
Hanqing Zhao <zhq2015@mail.ustc.edu.cn>
2022-07-26 02:06:54 +0800

    first commit

---
+++ configs/_ours_/faster_rcnn_r50_fpn_2x_lvis_328_baseline.py
@@ -0,0 +1,36 @@
+_base_ = [
+    '../_base_/models/faster_rcnn_r50_fpn.py',
+    '../_ours_/lvis_v1_longtail_328.py',
+    '../_base_/schedules/schedule_2x.py', '../_base_/default_runtime.py'
+]
+from sklearn.preprocessing import scale
+from mmdet.datasets.builder import PIPELINES
+
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(num_classes=328)),
+    test_cfg=dict(
+        rcnn=dict(
+            score_thr=0.0001,
+            # LVIS allows up to 300
+            max_per_img=300)))
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=False),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736),
+                   (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+data = dict(train=dict(dataset=dict(pipeline=train_pipeline)))
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_2x_coco/faster_rcnn_r50_fpn_2x_coco_bbox_mAP-0.384_20200504_210434-a5d8aa15.pth'
---
+++ configs/_ours_/lvis_v1_longtail_328.py
@@ -0,0 +1,31 @@
+# dataset settings
+_base_ = 'coco_instance.py'
+dataset_type = 'LVISV1Dataset'
+data_root = '/mnt/data/LVIS'
+import json
+with open('/mnt/home/syn4det/GLIDE/LVIS_gen_FG/results.json') as f:
+    classes = json.load(f)
+    classes= [i['name'] for i in classes]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        _delete_=True,
+        type='ClassBalancedDataset',
+        oversample_thr=1e-3,
+        dataset=dict(
+            classes=classes,
+            type=dataset_type,
+            ann_file=data_root + 'annotations/lvis_v1_train.json',
+            img_prefix=data_root)),
+    val=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v1_val.json',
+        img_prefix=data_root),
+    test=dict(
+        classes=classes,
+        type=dataset_type,
+        ann_file=data_root + 'annotations/lvis_v1_val.json',
+        img_prefix=data_root))
+evaluation = dict(metric=['bbox'])
